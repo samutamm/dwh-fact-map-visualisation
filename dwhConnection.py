6f15896d3bf74041b347b71b8d0ca8ba2b88b9d7
samutamm <samutamm@gmail.com>
2018-04-20 12:44:43 +0200

    take other fact also

--- dwhConnection.py
+++ dwhConnection.py
@@ -11,16 +11,18 @@ def create_connection():
                              cursorclass=pymysql.cursors.DictCursor)
 
 def get_tuples(fact, year):
+    assert fact == "deficit" or fact == "nbMeurtre"
     connection = create_connection()
     try:
         with connection.cursor() as cursor:
             # Create a new record
-            query = "select p.nom, e.deficit from EconomieStat as e \
+            table = "EconomieStat" if fact == "deficit" else "CrimeStat"
+            query = "select p.nom, e."+fact+" from "+table+" as e \
                         inner join Pays as p on e.idPays = p.idPays \
                         inner join Date as d on e.idDate = d.idDate \
                         WHERE d.annee = %s;"
             cursor.execute(query, (year,))
-            result = [[row['nom'], row['deficit']] for row in cursor]
+            result = [[row['nom'], row[fact]] for row in cursor]
             return result
 
         # connection is not autocommit by default. So you must commit to save
